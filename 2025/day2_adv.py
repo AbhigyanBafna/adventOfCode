553dc60088e98c5b64836a4e9d1251114550ef24
AbhigyanBafna <abhigyanbafna7@gmail.com>
2025-12-05 16:27:49 +0530

    Small Optimization

--- 2025/day2_adv.py
+++ 2025/day2_adv.py
@@ -8,28 +8,32 @@ ans = 0
 for item in arr:
     for i in range(item[0], item[1]+1):
         num = str(i)
-        print("Running for num : ", num)
+        # print("Running for num : ", num)
 
         n = len(num)
         half = n // 2
         for j in range(1,half+1):
             cand = num[:j]
-            print("Current Candidate and j : ", cand, " ", j)
             step = len(cand)
+            if n % step != 0:
+                continue
+
+            # print("Current Candidate and j : ", cand, " ", j)
+            
             k = j
             validCand = True
 
             while k<=n and k + step <= n:
                 sliceAt = k + step
                 nextCand = num[k:sliceAt]
-                print("NextCandidate against Candidate is ", nextCand)
+                # print("NextCandidate against Candidate is ", nextCand)
                 if cand != nextCand:
                     validCand = False
                     break
                 k += step
 
-            if validCand and k == n:
-                print("Selected Cand : ", i)
+            if validCand:
+                # print("Selected Cand : ", i)
                 ans += i
                 break
     
